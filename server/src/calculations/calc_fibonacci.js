3bee6da2cada4bfd7c07ac58938209d9febd4ea9
Aveerr <darkfull2004@gmail.com>
2025-05-06 12:13:06 +0300

    feat: валидация для расчёта фибоначи

--- server/src/calculations/calc_fibonacci.js
+++ server/src/calculations/calc_fibonacci.js
@@ -1,5 +1,11 @@
 module.exports = {
     calc: (n) => {
+        if (typeof n !== 'number' || !Number.isInteger(n)) 
+            throw new TypeError('Параметр n должен быть числом');
+
+        if (n < 0)
+            throw new RangeError('Факториал отрицательного числа не определен');
+        
         let container = [0, 1]; 
 
         for (let i = 2; i < n; i++) {
