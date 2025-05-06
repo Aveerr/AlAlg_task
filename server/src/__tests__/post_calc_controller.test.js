5f88e6fe5dd6b5c063f1cba1cac625654bf25247
Aveerr <darkfull2004@gmail.com>
2025-05-06 15:54:53 +0300

    test: Добавлен тест на проверку вычисления чисел фибоначи

--- server/src/__tests__/post_calc_controller.test.js
+++ server/src/__tests__/post_calc_controller.test.js
@@ -3,7 +3,7 @@ const app = require('../app');
 
 describe('POST /calculate/', () => {
     // Тест 1: Проверка пустого тела запроса
-    it('should return 400 for empty request body', async () => {
+    it('Должен вернуть ошибку 400 на пустое тело', async () => {
         const response = await request(app)
             .post('/calculate/')
             .send({})
@@ -14,18 +14,29 @@ describe('POST /calculate/', () => {
             "Параметр n обязателен"
         ]});
     });
-    it('should return 400 for invalid base schema', async () => {
+    it('Должен обработать неправильй тип данных', async () => {
         const response = await request(app)
             .post('/calculate/')
-            .send({ n: 'fa', calc_type: 'err' })
+            .send({ n: 'fa', calc_type: 'calc_factorial' })
             .expect(400);
         
         expect(response.body.errors).toBeDefined();
         expect(response.body.errors.length).toBeGreaterThan(0);
     });
+    it('Должен обработать неизвестный тип вычисления', async () => {
+        const response = await request(app)
+          .post('/calculate')
+          .send({ n: 5, calc_type: 'unknown_type' })
+          .expect(400); 
+        
+        expect(response.body).toEqual({
+            "errors": [
+                "Недопустимый тип расчета",
+        ]});
+    });
 
-    describe('factorial calculations', () => {
-        it('should calculate factorial for valid input', async () => {
+    describe('Вычисление факториала', () => {
+        it('Должен вычислить числа факториала с верными данными на входе', async () => {
           const response = await request(app)
             .post('/calculate/')
             .send({ n: 5, calc_type: 'calc_factorial' })
@@ -34,7 +45,7 @@ describe('POST /calculate/', () => {
           expect(response.body.result).toStrictEqual([1, 2, 6, 24, 120]);
         });
     
-        it('should return 400 for invalid factorial input', async () => {
+        it('Должен вывести ошибку 400 на неправильно значение n', async () => {
           const response = await request(app)
             .post('/calculate/')
             .send({ n: -5, calc_type: 'calc_factorial' })
@@ -43,4 +54,24 @@ describe('POST /calculate/', () => {
           expect(response.body.errors).toBeDefined();
         });
     });
+
+    describe('Вычисление фибоначи', () => {
+        it('Должен вычислить числа фибоначи с верными данными на входе', async () => {
+          const response = await request(app)
+            .post('/calculate')
+            .send({ n: 10, calc_type: 'calc_fibonacci' })
+            .expect(200);
+          
+          expect(response.body.result).toStrictEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]); 
+        });
+    
+        it('Должен вывести ошибку 400 на неверное значение n', async () => {
+          const response = await request(app)
+            .post('/calculate')
+            .send({ n: -1, calc_type: 'calc_fibonacci' })
+            .expect(400);
+          
+          expect(response.body.errors).toBeDefined();
+        });
+    });5
 })
--- server/src/calculations/calc_fibonacci.js
+++ server/src/calculations/calc_fibonacci.js
@@ -8,7 +8,7 @@ module.exports = {
         
         let container = [0, 1]; 
 
-        for (let i = 2; i < n; i++) {
+        for (let i = 2; i < n+1; i++) {
             let nextNum = container[i - 1] + container[i - 2];
             container.push(nextNum);
         }
